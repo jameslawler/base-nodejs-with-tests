c0829708744d84dc54012b348645645a16680a59
James Lawler <james.lawler@check24.de>
2020-07-03 14:03:12 +0200

    Upgrade to jest

--- index.spec.js
+++ index.spec.js
@@ -1,11 +1,9 @@
-const { expect } = require('chai');
-
 const test = require('./index');
 
 describe('test', () => {
   describe('when true', () => {
     it('should return true', () => {
-      expect(test(true)).to.be.true;
+      expect(test(true)).toBe(true);
     });
-  })
+  });
 });
